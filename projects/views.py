89f3307cde082b649f21d613a0da3c3d64c7ccfa
Joshuamjv2 <joshuamjv22@gmail.com>
2021-05-20 15:27:04 +0300

    home page projects section

--- projects/views.py
+++ projects/views.py
@@ -4,9 +4,10 @@ from .models import Project
 # Create your views here.
 
 def home(request):
-    return render(request, 'home/index.html')
+    projects = Project.objects.filter(featured=True)
+    return render(request, 'home/index.html', {'projects':projects})
 
 
 def all_projects(request):
-    projects = Project.objects.all()
+    projects = Project.objects.filter(featured=True)
     return render(request, 'project.html', {'projects':projects})
