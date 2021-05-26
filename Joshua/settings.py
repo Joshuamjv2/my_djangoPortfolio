9b2fade6d841c30e81963791ac3c1341a4a89ee4
Joshuamjv2 <joshuamjv22@gmail.com>
2021-05-26 13:39:30 +0300

    bad request 2

--- Joshua/settings.py
+++ Joshua/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-0te^0-v_r6&4#la4s-nor531$-h$p(@=9-afp1(rdbnj8p6zh^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['muwanguzi-joshua.herokuapp.com']
 
 
 # Application definition
