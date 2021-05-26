4541d66c20d19ccd059b89e675d8c5f32d27186f
Joshuamjv2 <joshuamjv22@gmail.com>
2021-05-26 13:36:35 +0300

    see server

--- Joshua/settings.py
+++ Joshua/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0te^0-v_r6&4#la4s-nor531$-h$p(@=9-afp1(rdbnj8p6zh^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = []
 
 
 # Application definition
