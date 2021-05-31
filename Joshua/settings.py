c46d22ee58b22cf3af26d08a5f40f49dbb209db4
Joshuamjv2 <joshuamjv22@gmail.com>
2021-05-31 03:48:48 +0300

    removed .evn file for production

--- Joshua/settings.py
+++ Joshua/settings.py
@@ -11,9 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
+# from decouple import config
 import os
-import django_heroku
+# import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-0te^0-v_r6&4#la4s-nor531$-h$p(@=9-afp1(rdbnj8p6zh^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
