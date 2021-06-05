fc1448b98c2901316ed084e9c5e64a55cce09496
Joshuamjv2 <joshuamjv22@gmail.com>
2021-06-05 04:00:22 +0300

    testing animations

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,5 @@
 const general = document.querySelectorAll('.anim');
-
+const specials = document.querySelectorAll('.special-bits');
 const options = {
     root: null,
     threshold: .5,
@@ -9,11 +9,29 @@ const options = {
 const general_observer = new IntersectionObserver((entries, general_observer)=>{
     entries.forEach(entry =>{
         if (entry.isIntersecting){
-            entry.target.style.animation = 'gen_anim 2s ease-in-out forwards'
+            entry.target.style.animation = `gen_anim 1s ease-in-out forwards`
         }
     })
 }, options)
 
 general.forEach(anim=>{
     general_observer.observe(anim)
+})
+
+const sp_options = {
+    root: null,
+    threshold: 1,
+    rootMargin: "0px"
+}
+
+const bits_observer = new IntersectionObserver((entries, bits_observer)=>{
+    entries.forEach(entry=>{
+        if (entry.isIntersecting){
+            entry.target.style.animation = `specials 500ms ${entry.target.dataset.delay} ease-in-out forwards`
+        }
+    })
+}, sp_options)
+
+specials.forEach(special=>{
+    bits_observer.observe(special)
 })
