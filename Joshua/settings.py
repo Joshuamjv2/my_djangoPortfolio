220c2c5ca31380b55a128db37ceaa279d2908923
Joshuamjv2 <joshuamjv22@gmail.com>
2021-06-11 00:55:12 +0300

    real secret key

--- Joshua/settings.py
+++ Joshua/settings.py
@@ -25,16 +25,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = env('SECRET_KEY')
-# SECRET_KEY = env('SECRET_KEy')
-SECRET_KEY=env('SECRET_KEY')
+
+
+
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = ['muwanguzi-joshua.herokuapp.com']
+
+SECRET_KEY = "e52da42371d0b34b14aa8063c7d617bcc3536d35f259a9db97fd4291466f3de1"
 
 
 # Application definition
