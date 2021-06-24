7c87feb96051c902214bfbcc906b850ec1ff4ddb
Joshuamjv2 <joshuamjv22@gmail.com>
2021-06-24 14:37:23 +0300

    mission not accomplished

--- Joshua/settings.py
+++ Joshua/settings.py
@@ -6,7 +6,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import logging
 from pathlib import Path
 from distutils.util import strtobool
 from dotenv import load_dotenv
